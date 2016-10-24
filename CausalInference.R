32ba52f07c4deb59a105561ea5b52ba095201c7d
Hui Hu <benhhu@gmail.com>
2016-10-24 14:28:06 -0400

    codes updated

--- CausalInference.R
+++ CausalInference.R
@@ -1,4 +1,14 @@
 #Causal Inference
+library(sas7bdat)
+library(car)
 
+setwd("//file.phhp.ufl.edu/home/huihu/files/RWD/R-Causal-Inference")
+nhefs <- read.sas7bdat("nhefs_book.sas7bdat")
+nhefs$cens <- as.numeric(is.na(nhefs$wt82))
+nhefs$older <- as.numeric(nhefs$age > 50 & !is.na(nhefs$age))
+
+
+nhefs$education.code <- recode(nhefs$school, " 0:8 = 1; 9:11 = 2; 12 = 3; 13:15 = 4; 16:hi = 5; NA = 6 ")
+nhefs$education <- recode(nhefs$education.code, " 1 = '1. 8th grade or less'; 2 = '2. HS dropout'; 3 = '3. HS'; 4 = '4. College dropout'; 5 = '5. College or more'; 6 = 'Unknown' ")
 
 #IP weighting
