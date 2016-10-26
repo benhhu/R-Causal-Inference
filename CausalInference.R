baf6858aa771a386aaf5529ca4fa52dfa85369b9
Hui Hu <benhhu@gmail.com>
2016-10-26 10:52:26 -0400

    standardization added

--- CausalInference.R
+++ CausalInference.R
@@ -4,6 +4,7 @@
 #install.packages("BSagri")
 #install.packages("sandwich")
 #install.packages("descr")
+#install.packages("boot")
 library(sas7bdat)
 library(car)
 library(geepack)
@@ -11,6 +12,7 @@ library(multcomp)
 library(BSagri)
 library(sandwich)
 library(descr)
+library(boot)
 
 setwd("//file.phhp.ufl.edu/home/huihu/files/RWD/R-Causal-Inference")
 nhefs <- read.sas7bdat("nhefs_book.sas7bdat")
@@ -201,7 +203,96 @@ round(cbind(beta, SE, lcl, ucl, p.value),1)[2,]
 #Standardization and G-Formula#
 ###############################
 
+#Estimating the Mean Outcome via Modeling
+# Analysis restricted to N=1566 
+# with non-missing values in the following covariates
+nhefs.original <- nhefs # Original data saved for later use
+nhefs$id <- 1:nrow(nhefs)
+nhefs2 <- nhefs[c("id", "qsmk", "sex", "race", "age", "school", "smokeintensity", "smokeyrs", "exercise", "active", "wt71", "wt82")]
+dim(nhefs2)
+# restricting data for non-missing
+nhefs2 <- as.data.frame(na.omit(nhefs2))
+dim(nhefs2)
+nhefs.id.matched <- subset(nhefs, id %in% nhefs2$id)
+dim(nhefs)
+# restricting data for uncensored, for comparison with observed outcome
+nhefs0 <- subset(nhefs.id.matched, cens == 0)
+dim(nhefs0)
+table(nhefs0$qsmk) # untreated vs treated
+# Estimates
+glm.obj <- glm(wt82_71~ as.factor(qsmk) + as.factor(sex) + as.factor(race) + age + I(age^2) + as.factor(education) + smokeintensity + I(smokeintensity^2) + smokeyrs + I(smokeyrs^2) + as.factor(exercise) + as.factor(active) + wt71 + I(wt71^2) + I(qsmk*smokeintensity), data = nhefs0)
+nhefs0$meanY <- predict(glm.obj, type = "response")
+# print variable values corresponding to subject with unique identifier 24770
+nhefs0[nhefs0$seqn ==24770,c("meanY", "qsmk", "sex", "race", "age", "education","smokeintensity", "smokeyrs", "exercise", "active","wt71")]
+summary(nhefs0$meanY)
+summary(nhefs0$wt82_71)
 
+#Standardizing the Mean Outcome to the Confounder Distribution
+# 1st copy: equal to original one
+nhefs0$interv <- rep(-1, nrow(nhefs0))
+dim(nhefs0)
+# 2nd copy: treatment set to 0, outcome to missing
+nhefs.untr <- nhefs0
+nhefs.untr$interv <- rep(0, nrow(nhefs.untr))
+nhefs.untr$qsmk <- rep(0, nrow(nhefs.untr))
+nhefs.untr$wt82_71 <- rep(NA, nrow(nhefs.untr))
+dim(nhefs.untr)
+# 3rd copy: treatment set to 1, outcome to missing
+nhefs.tr <- nhefs0
+nhefs.tr$interv <- rep(1, nrow(nhefs.tr))
+nhefs.tr$qsmk <- rep(1, nrow(nhefs.tr))
+nhefs.tr$wt82_71 <- rep(NA, nrow(nhefs.tr))
+dim(nhefs.tr)
+# create a dataset with 3 copies of each subject
+onesample <- as.data.frame(rbind(nhefs0, nhefs.untr, nhefs.tr))
+dim(onesample)
+# Estimates
+# linear model to estimate mean outcome conditional on treatment & confounders,
+# parameters are estimated using original observations only (interv= -1),
+# parameter estimates are used to predict mean outcome for observations 
+# with treatment set to 0 (interv=0) and to 1 (innterv=1);
+glm.obj <- glm(wt82_71~ as.factor(qsmk) + as.factor(sex) + as.factor(race) + age + I(age^2) + as.factor(education) + smokeintensity + I(smokeintensity^2) + smokeyrs + I(smokeyrs^2) + as.factor(exercise) + as.factor(active) + wt71 + I(wt71^2), data = onesample)
+onesample$meanY <- predict(glm.obj, onesample, type = "response")
+# estimate mean outcome in each of the groups interv=0, and interv=1;
+# this mean outcome is a weighted average of the mean outcomes 
+# in each combination of values of treatment and confounders, 
+# that is, the standardized outcome;
+with(onesample, tapply(meanY, list(interv), mean))
+
+#Use Bootstrap to Compute the 95% CI
+# Compute basic bootstrap confidence interval.
+# Below is the function to estimate mean outcome in each of the groups 
+# interv=-1, interv=0 & interv=1 conditional on treatment and confounders,
+# also include the mean outcome of interv = 1 vs. interv = 0
+boot.fun <- function(dat, index, type){
+  sampled.data <- dat[index,]
+  fit <- glm(formula = wt82_71~ as.factor(qsmk) + as.factor(sex) + as.factor(race) + 
+               age + I(age^2) + as.factor(education) + smokeintensity + I(smokeintensity^2) + 
+               smokeyrs + I(smokeyrs^2) + as.factor(exercise) + as.factor(active) + wt71 + 
+               I(wt71^2), data = sampled.data)
+  sampled.data$meanY <- predict(fit, sampled.data, type = "response")
+  std.mean <- with(sampled.data, tapply(meanY, list(interv), mean))
+  mean.list <- as.numeric(unlist(c(std.mean, diff(std.mean[c(2,3)]))))
+  return(mean.list)
+}
+# Parametric g-formula: Bootstrap results using nboot samples
+nboot <- 100
+# number of bootstrap samples to run
+# creation of bootstarp samples using fixed seed to reproduce results
+set.seed(1232)
+bootres <- boot(onesample, boot.fun, R = nboot)
+bootres
+nrow(bootres$t)
+boot.stat <- matrix(NA, 4, 4)
+for (i in 1:4){
+  boot.stat[i,] <- c(bootres$t0[i], sd(bootres$t[,i]), 
+                     bootres$t0[i] - 1.96 * sd(bootres$t[,i]), 
+                     bootres$t0[i] + 1.96 * sd(bootres$t[,i]))
+}
+dimnames(boot.stat) <- list(c("observed", "No treatment", 
+                              "Treatment", "Difference"), 
+                            c("mean", "sd", "LCL", "UCL"))
+boot.stat
 
 
 
